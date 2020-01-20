d91e339e5a022cd1f3e15aa0450b3c0f667848a5
alumno <alumno@E128>
2020-01-20 12:49:39 -0700

    asdf

--- reproductor/MainWindow.xaml.cs
+++ reproductor/MainWindow.xaml.cs
@@ -18,13 +18,20 @@ using Microsoft.Win32;
 using NAudio.Wave;
 using NAudio.Wave.SampleProviders;
 
+using System.Windows.Threading;
+
 namespace reproductor
 {
+    
+
     /// <summary>
     /// Lógica de interacción para MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        DispatcherTimer timer;
+
         //Lector de archivos
         AudioFileReader reader;
         //Comunicacion con la tarjeta de audio
@@ -38,6 +45,17 @@ namespace reproductor
             btnRerpoducir.IsEnabled = false;
             btnDetener.IsEnabled = false;
             btnPausa.IsEnabled = false;
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(500);
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            lblTiempoActual.Text = reader.CurrentTime.ToString().Substring(0, 8);
+
+            
         }
 
         void ListarDisposSalida()
@@ -97,12 +115,16 @@ namespace reproductor
                     btnDetener.IsEnabled = true;
 
                     lblTiempoTotal.Text = reader.TotalTime.ToString().Substring(0, 8);
+                    lblTiempoActual.Text = reader.CurrentTime.ToString().Substring(0, 8);
+
+                    timer.Start();
                 }
             }
         }
 
         private void Output_PlaybackStopped(object sender, StoppedEventArgs e)
         {
+            timer.Stop();
             reader.Dispose();
             output.Dispose();
         }
